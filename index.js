01bdab54f395c173a29963442e1b810973edc971
mehedi-hasan-saikat <m.h.saikat15@gmail.com>
2022-02-18 02:12:31 +0600

    update

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.24hkl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 // console.log('mogno  :',uri);
