b6a2f4acb903f4aa810dbdf273079f1f16f8b9e8
Dev70 <Shanto9585>
2022-03-06 18:44:10 +0600

    update

--- index.js
+++ index.js
@@ -33,49 +33,49 @@ async function run() {
 
 
 
-        // get question  solve
+    // get question  solve
 
-        app.get('/getBlogComment', async (req, res) => {
+    app.get('/getBlogComment', async (req, res) => {
 
-          const result = await BlogCommentCollection.find({}).toArray()
-          res.send(result)
-      })
-
-
-
-   // post blog comment 
-   app.post('/PostBlogComment', async (req, res) => {
-    const BlogComment = req.body;
-    const result = await BlogCommentCollection.insertOne(BlogComment);
-    res.json(result);
-    console.log(result)
-
-  });
+      const result = await BlogCommentCollection.find({}).toArray()
+      res.send(result)
+    })
 
 
+
+    // post blog comment 
+    app.post('/PostBlogComment', async (req, res) => {
+      const BlogComment = req.body;
+      const result = await BlogCommentCollection.insertOne(BlogComment);
+      res.json(result);
+      console.log(result)
+
+    });
 
 
 
 
 
 
-   // POST solve
-   app.post('/addQuestionSolve', async (req, res) => {
-    const questionSolve = req.body;
-    const result = await questionSolveCollection.insertOne(questionSolve);
-    res.json(result);
-    console.log(result)
-
-  });
 
 
+    // POST solve
+    app.post('/addQuestionSolve', async (req, res) => {
+      const questionSolve = req.body;
+      const result = await questionSolveCollection.insertOne(questionSolve);
+      res.json(result);
+      console.log(result)
 
-        // get question  solve
+    });
 
-        app.get('/questionSolve/${id}', async (req, res) => {
-          const result = await questionSolveCollection.find({ questionId: req.params.id }).toArray()
-          res.send(result)
-      })
+
+
+    // get question  solve
+
+    app.get('/questionSolve/${id}', async (req, res) => {
+      const result = await questionSolveCollection.find({ questionId: req.params.id }).toArray()
+      res.send(result)
+    })
 
 
 
@@ -125,18 +125,18 @@ async function run() {
 
 
     // blog update status 
-    
+
     app.put("/BookStatusUpdate/:id", async (req, res) => {
 
       const filter = { _id: ObjectId(req.params.id) };
 
       const result = await allBooksCollection.updateOne(filter, {
-          $set: {
-              status: req.body.status,
-          },
+        $set: {
+          status: req.body.status,
+        },
       });
       res.send(result);
-  });
+    });
 
 
 
@@ -171,22 +171,22 @@ async function run() {
 
 
     // blog update status 
-    
+
     app.put("/BlogStatusUpdate/:id", async (req, res) => {
 
       const filter = { _id: ObjectId(req.params.id) };
 
       const result = await allBlogsCollection.updateOne(filter, {
-          $set: {
-              status: req.body.status,
-          },
+        $set: {
+          status: req.body.status,
+        },
       });
       res.send(result);
-  });
+    });
 
 
-  
-     
+
+
 
 
     // POST notes
@@ -207,23 +207,18 @@ async function run() {
 
 
     // blog update status 
-    
+
     app.put("/notesStatusUpdate/:id", async (req, res) => {
 
       const filter = { _id: ObjectId(req.params.id) };
 
       const result = await allNotesCollection.updateOne(filter, {
-          $set: {
-              status: req.body.status,
-          },
+        $set: {
+          status: req.body.status,
+        },
       });
       res.send(result);
-  });
-
-
-
-
-
+    });
 
 
     // add user 
@@ -268,55 +263,55 @@ async function run() {
 
 
 
- // get my note
+    // get my note
 
     app.get('/myQuestions/:email', async (req, res) => {
-    const result = await allQuestionsCollection.find({ email: req.params.email }).toArray()
-    res.send(result)
-          })
+      const result = await allQuestionsCollection.find({ email: req.params.email }).toArray()
+      res.send(result)
+    })
 
-          
- // // get my note
+
+    // // get my note
 
     app.get('/myNotes/:email', async (req, res) => {
-    const result = await allNotesCollection.find({ email: req.params.email }).toArray()
-    res.send(result)
-          })
+      const result = await allNotesCollection.find({ email: req.params.email }).toArray()
+      res.send(result)
+    })
 
 
- // // get my Books
+    // // get my Books
 
     app.get('/myBooks/:email', async (req, res) => {
-    const result = await allBooksCollection.find({ email: req.params.email }).toArray()
-    res.send(result)
-          })
+      const result = await allBooksCollection.find({ email: req.params.email }).toArray()
+      res.send(result)
+    })
 
 
- // // get my blogs
+    // // get my blogs
 
     app.get('/myBlogs/:email', async (req, res) => {
-    const result = await allBlogsCollection.find({ email: req.params.email }).toArray()
-    res.send(result)
-          })
+      const result = await allBlogsCollection.find({ email: req.params.email }).toArray()
+      res.send(result)
+    })
     //MAKE ADMIN
-    app.put('/users/admin', async(req, res)=>{
+    app.put('/users/admin', async (req, res) => {
       const user = req.body;
-      const filter = {email : user.email};
-      const updateDoc = {$set:{role : 'admin'}};
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: 'admin' } };
       const result = await userCollection.updateOne(filter, updateDoc)
       res.json(result)
     })
 
     //ADMIN CONDITIONALLY RENDERED
-    app.get('/users/:email', async(req, res) =>{
-          const email = req.params.email;
-          const query = {email: email};
-          const user = await userCollection.findOne(query)
-          let isAdmin = false;
-          if(user?.role === 'admin'){
-              isAdmin = true;
-          }
-          res.json({admin: isAdmin});
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query)
+      let isAdmin = false;
+      if (user?.role === 'admin') {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
     })
 
 
