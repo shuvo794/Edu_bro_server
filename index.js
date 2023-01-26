9488c44a52478eb47c3b05c2e34d422f410b82cb
shuvo794 <shuvohossain794@gmail.com>
2023-01-26 09:23:59 +0600

    upadate server site

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // console.log('mogno:',uri);
 const client = new MongoClient(uri, {
-  // @ts-ignore
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
