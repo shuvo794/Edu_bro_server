3c805c698bf12fc36b8e1ec65c174518005cd6f8
shuvo794 <shuvohossain794@gmail.com>
2023-01-26 09:12:05 +0600

    update

--- index.js
+++ index.js
@@ -34,126 +34,94 @@ async function run() {
     const questionSolveCollection = database.collection("questionSolve");
     const BlogCommentCollection = database.collection("BlogComment");
 
-
-
     // get question  solve
 
-    app.get('/getBlogComment', async (req, res) => {
+    app.get("/getBlogComment", async (req, res) => {
+      const result = await BlogCommentCollection.find({}).toArray();
+      res.send(result);
+    });
 
-      const result = await BlogCommentCollection.find({}).toArray()
-      res.send(result)
-    })
-
-
-
-    // post blog comment 
-    app.post('/PostBlogComment', async (req, res) => {
+    // post blog comment
+    app.post("/PostBlogComment", async (req, res) => {
       const BlogComment = req.body;
       const result = await BlogCommentCollection.insertOne(BlogComment);
       res.json(result);
-      console.log(result)
-
+      console.log(result);
     });
 
-
-
-
-
-
-
-
     // POST solve
-    app.post('/addQuestionSolve', async (req, res) => {
+    app.post("/addQuestionSolve", async (req, res) => {
       const questionSolve = req.body;
       const result = await questionSolveCollection.insertOne(questionSolve);
       res.json(result);
-      console.log(result)
-
+      console.log(result);
     });
-
-
 
     // get question  solve
 
-    app.get('/questionSolve/:id', async (req, res) => {
-      const result = await questionSolveCollection.find({ questionId: req.params.id }).toArray()
-      res.send(result)
-    })
-
-
+    app.get("/questionSolve/:id", async (req, res) => {
+      const result = await questionSolveCollection
+        .find({ questionId: req.params.id })
+        .toArray();
+      res.send(result);
+    });
 
     // POST Question
-    app.post('/postQuestion', async (req, res) => {
+    app.post("/postQuestion", async (req, res) => {
       const allQuestions = req.body;
       const result = await allQuestionsCollection.insertOne(allQuestions);
       res.json(result);
-      console.log(result)
-
+      console.log(result);
     });
-
-
-
-    // Get all questions api 
-    // app.get("/allQuestions", async (req, res) => {
-    //   const cursor = allQuestionsCollection.find({});
-    //   const allQuestions = await cursor.toArray();
-    //   res.send(allQuestions);
-    // });
-
-
-
-
-    // Get all questions api 
 
     app.get("/allQuestions", async (req, res) => {
       const page = req.query.page;
       const size = parseInt(req.query.size);
       const query = req.query;
-      delete query.page
-      delete query.size
-      Object.keys(query).forEach(key => {
-        if (!query[key])
-          delete query[key]
+      delete query.page;
+      delete query.size;
+      Object.keys(query).forEach((key) => {
+        if (!query[key]) delete query[key];
       });
 
       if (Object.keys(query).length) {
         const cursor = allQuestionsCollection.find(query);
-        const count = await cursor.count()
-        const allQuestions = await cursor.skip(page * size).limit(size).toArray()
+        const count = await cursor.count();
+        const allQuestions = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
         res.json({
-          allQuestions, count
+          allQuestions,
+          count,
         });
       } else {
         const cursor = allQuestionsCollection.find({});
-        const count = await cursor.count()
-        const allQuestions = await cursor.skip(page * size).limit(size).toArray()
+        const count = await cursor.count();
+        const allQuestions = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
 
         res.json({
-          allQuestions, count
+          allQuestions,
+          count,
         });
       }
-
     });
 
-
-
-
-
     // get single questions
-    app.get('/question/:id', async (req, res) => {
+    app.get("/question/:id", async (req, res) => {
       const id = req.params.id;
-      const result = await allQuestionsCollection.findOne({ _id: ObjectId(id) })
-      res.json(result)
-    })
+      const result = await allQuestionsCollection.findOne({
+        _id: ObjectId(id),
+      });
+      res.json(result);
+    });
 
-
-
-
-
-    // blog update status 
+    // blog update status
 
     app.put("/QuestionStatusUpdate/:id", async (req, res) => {
-
       const filter = { _id: ObjectId(req.params.id) };
 
       const result = await allQuestionsCollection.updateOne(filter, {
@@ -164,21 +132,15 @@ async function run() {
       res.send(result);
     });
 
-
-
-
-
     // POST Books
-    app.post('/postBooks', async (req, res) => {
+    app.post("/postBooks", async (req, res) => {
       const allBooks = req.body;
       const result = await allBooksCollection.insertOne(allBooks);
       res.json(result);
-      console.log(result)
-
+      console.log(result);
     });
 
-
-    // Get all books api 
+    // Get all books api
     app.get("/allBooks", async (req, res) => {
       const cursor = allBooksCollection.find({});
       const allBooks = await cursor.toArray();
@@ -186,26 +148,23 @@ async function run() {
     });
 
     // POST syllabus
-    app.post('/postSyllabus', async (req, res) => {
+    app.post("/postSyllabus", async (req, res) => {
       const allSyllabus = req.body;
       const result = await allSyllabusCollection.insertOne(allSyllabus);
       res.json(result);
-      console.log(result)
-
+      console.log(result);
     });
 
-
-    // Get all syllabus api 
+    // Get all syllabus api
     app.get("/allSyllabus", async (req, res) => {
       const cursor = allSyllabusCollection.find({});
       const allSyllabus = await cursor.toArray();
       res.send(allSyllabus);
     });
 
-    // syllabus update status 
+    // syllabus update status
 
     app.put("/SyllabusStatusUpdate/:id", async (req, res) => {
-
       const filter = { _id: ObjectId(req.params.id) };
 
       const result = await allSyllabusCollection.updateOne(filter, {
@@ -216,13 +175,9 @@ async function run() {
       res.send(result);
     });
 
-
-
-
-    // blog update status 
+    // blog update status
 
     app.put("/BookStatusUpdate/:id", async (req, res) => {
-
       const filter = { _id: ObjectId(req.params.id) };
 
       const result = await allBooksCollection.updateOne(filter, {
@@ -232,9 +187,6 @@ async function run() {
       });
       res.send(result);
     });
-
-
-
 
     //add user review
     app.post("/review", async (req, res) => {
@@ -252,81 +204,68 @@ async function run() {
       res.send(reviews);
     });
 
-
-
-
-
     //get all Labs
     app.get("/allLabs", async (req, res) => {
-
-      const cursor = allLabsCollection.find({})
+      const cursor = allLabsCollection.find({});
       const page = req.query.page;
       const size = parseInt(req.query.size);
       let allLabs;
       const count = await cursor.count();
       if (page) {
-        allLabs = await cursor.skip(page * size).limit(size).toArray()
+        allLabs = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
       } else {
         allLabs = await cursor.toArray();
       }
       res.send({
         count,
-        allLabs
+        allLabs,
       });
     });
 
-
-
     //post Labs
-    app.post('/postLabs', async (req, res) => {
+    app.post("/postLabs", async (req, res) => {
       const allLabs = req.body;
       const result = await allLabsCollection.insertOne(allLabs);
       res.json(result);
-      console.log(result)
-
+      console.log(result);
     });
 
-
-    app.get('/myLabs/:email', async (req, res) => {
-      const result = await allLabsCollection.find({ email: req.params.email }).toArray()
-      res.send(result)
-    })
-
-
+    app.get("/myLabs/:email", async (req, res) => {
+      const result = await allLabsCollection
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+    });
 
     // POST blogs
-    app.post('/postBlogs', async (req, res) => {
+    app.post("/postBlogs", async (req, res) => {
       const allBlogs = req.body;
       const result = await allBlogsCollection.insertOne(allBlogs);
       res.json(result);
-      console.log(result)
-
+      console.log(result);
     });
 
-
-    // Get all blogs api 
+    // Get all blogs api
     app.get("/allBlogs", async (req, res) => {
       const cursor = allBlogsCollection.find({});
       const allBlogs = await cursor.toArray();
       res.send(allBlogs);
     });
 
-
-
     // get single blog
-    app.get('/blog-details/:id', async (req, res) => {
+    app.get("/blog-details/:id", async (req, res) => {
       const id = req.params.id;
       // @ts-ignore
-      const result = await allBlogsCollection.findOne({ _id: ObjectId(id) })
-      res.json(result)
-    })
+      const result = await allBlogsCollection.findOne({ _id: ObjectId(id) });
+      res.json(result);
+    });
 
-
-
-    // blog update status 
+    // blog update status
 
     app.put("/BlogStatusUpdate/:id", async (req, res) => {
-
       const filter = { _id: ObjectId(req.params.id) };
 
       const result = await allBlogsCollection.updateOne(filter, {
@@ -337,31 +276,24 @@ async function run() {
       res.send(result);
     });
 
-
-
-
-
     // POST notes
-    app.post('/postNotes', async (req, res) => {
+    app.post("/postNotes", async (req, res) => {
       const allNotes = req.body;
       const result = await allNotesCollection.insertOne(allNotes);
       res.json(result);
-      console.log(result)
-
+      console.log(result);
     });
 
-    // Get all notes api 
+    // Get all notes api
     app.get("/allNotes", async (req, res) => {
       const cursor = allNotesCollection.find({});
       const allNotes = await cursor.toArray();
       res.send(allNotes);
     });
 
-
-    // blog update status 
+    // blog update status
 
     app.put("/notesStatusUpdate/:id", async (req, res) => {
-
       const filter = { _id: ObjectId(req.params.id) };
 
       const result = await allNotesCollection.updateOne(filter, {
@@ -372,111 +304,108 @@ async function run() {
       res.send(result);
     });
 
-
-    // add user 
+    // add user
     app.post("/users", async (req, res) => {
       const result = await userCollection.insertOne(req.body);
       res.send(result);
-      console.log(result)
+      console.log(result);
     });
 
-
-    // upsert for google login 
-    app.put('/users', async (req, res) => {
+    // upsert for google login
+    app.put("/users", async (req, res) => {
       const user = req.body;
-      const filter = { email: user.email }
-      const options = { upsert: true }
-      const updateDoc = { $set: user }
-      const result = await userCollection.updateOne(filter, updateDoc, options)
-      res.json(result)
-    })
-
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+    });
 
     // update user
-    app.put('/updateUser', async (req, res) => {
+    app.put("/updateUser", async (req, res) => {
       const user = req.body;
-      const filter = { email: user.email }
+      const filter = { email: user.email };
       const updateDoc = {
         $set: {
           department: user.department,
-          university: user.university
-        }
-      }
-      const result = await userCollection.updateOne(filter, updateDoc)
-      res.json(result)
-    })
+          university: user.university,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
 
     // get single user
-    app.get('/users/:email', async (req, res) => {
-      const email = req.params.email
-      const query = { email: email }
-      const user = await userCollection.findOne(query)
-      res.json(user)
-    })
-
-
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      res.json(user);
+    });
 
     // get my note
 
-    app.get('/myQuestions/:email', async (req, res) => {
-      const result = await allQuestionsCollection.find({ email: req.params.email }).toArray()
-      res.send(result)
-    })
-
+    app.get("/myQuestions/:email", async (req, res) => {
+      const result = await allQuestionsCollection
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+    });
 
     // // get my note
 
-    app.get('/myNotes/:email', async (req, res) => {
-      const result = await allNotesCollection.find({ email: req.params.email }).toArray()
-      res.send(result)
-    })
-
+    app.get("/myNotes/:email", async (req, res) => {
+      const result = await allNotesCollection
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+    });
 
     // // get my Books
 
-    app.get('/myBooks/:email', async (req, res) => {
-      const result = await allBooksCollection.find({ email: req.params.email }).toArray()
-      res.send(result)
-    })
+    app.get("/myBooks/:email", async (req, res) => {
+      const result = await allBooksCollection
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+    });
     // // get my syllabus
 
-    app.get('/mySyllabus/:email', async (req, res) => {
-      const result = await allSyllabusCollection.find({ email: req.params.email }).toArray()
-      res.send(result)
-    })
-
+    app.get("/mySyllabus/:email", async (req, res) => {
+      const result = await allSyllabusCollection
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+    });
 
     // // get my blogs
 
-    app.get('/myBlogs/:email', async (req, res) => {
-      const result = await allBlogsCollection.find({ email: req.params.email }).toArray()
-      res.send(result)
-    })
+    app.get("/myBlogs/:email", async (req, res) => {
+      const result = await allBlogsCollection
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+    });
     //MAKE ADMIN
-    app.put('/users/admin', async (req, res) => {
+    app.put("/users/admin", async (req, res) => {
       const user = req.body;
       const filter = { email: user.email };
-      const updateDoc = { $set: { role: 'admin' } };
-      const result = await userCollection.updateOne(filter, updateDoc)
-      res.json(result)
-    })
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
 
     //ADMIN CONDITIONALLY RENDERED
-    app.get('/users/:email', async (req, res) => {
+    app.get("/users/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
-      const user = await userCollection.findOne(query)
+      const user = await userCollection.findOne(query);
       let isAdmin = false;
-      if (user?.role === 'admin') {
+      if (user?.role === "admin") {
         isAdmin = true;
       }
       res.json({ admin: isAdmin });
-    })
-
-
-
-
-
+    });
   } finally {
     // await client.close()
   }
@@ -488,7 +417,8 @@ app.get("/", (req, res) => {
   res.send("Running The Edu-Bro Server");
 });
 
-
 app.listen(port, () => {
   console.log("Running server is port", port);
 });
+
+
